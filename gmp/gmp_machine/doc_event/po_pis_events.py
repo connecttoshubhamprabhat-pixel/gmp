e39086bdabdb1ffa2da7e66a1db24eb5c75708b0
connecttoshubhamprabhat-pixel <connecttoshubhamprabhat@gmail.com>
2026-07-14 18:55:19 +0530

    feat updated po_pis event code

--- gmp/gmp_machine/doc_event/po_pis_events.py
+++ gmp/gmp_machine/doc_event/po_pis_events.py
@@ -28,53 +28,142 @@ def after_rename_po(self, method, olddn, newdn, merge=False):
 	""", (newdn, olddn))
 
 
+# def validate_delete_po(self, method):
+# 	"""
+# 	Before Purchase Order is deleted, check if its name is present
+# 	in 'purchase_order_duplicate' of any Payment Intimation Slip.
+# 	If found, block the deletion.
+# 	"""
+# 	linked = frappe.db.sql("""
+# 		SELECT name FROM `tabPayment Intimation Slip`
+# 		WHERE purchase_order_duplicate = %s
+# 	""", (self.name,))
+
+# 	if linked:
+# 		frappe.throw(
+# 			"Cannot delete Purchase Order {0}, because it's linked with Payment Intimation Slip {1}".format(
+# 				frappe.bold(self.name), frappe.bold(linked[0][0])
+# 			)
+# 		)
+		
+
+
+
+
+
+
+
+
+import frappe
+
 def validate_delete_po(self, method):
 	"""
-	Before Purchase Order is deleted, check if its name is present
-	in 'purchase_order_duplicate' of any Payment Intimation Slip.
-	If found, block the deletion.
+	Prevent deletion of an original PO if linked with Payment Intimation Slip.
+	If an amended PO is being deleted, move the link back to the original PO.
 	"""
-	linked = frappe.db.sql("""
-		SELECT name FROM `tabPayment Intimation Slip`
-		WHERE purchase_order_duplicate = %s
-	""", (self.name,))
 
-	if linked:
-		frappe.throw(
-			"Cannot delete Purchase Order {0}, because it's linked with Payment Intimation Slip {1}".format(
-				frappe.bold(self.name), frappe.bold(linked[0][0])
-			)
+	if not self.amended_from:
+		linked = frappe.get_all(
+			"Payment Intimation Slip",
+			filters={"purchase_order_duplicate": self.name},
+			pluck="name"
 		)
-		
+
+		if linked:
+			frappe.throw(
+				"Cannot delete Purchase Order {0} because it is linked with Payment Intimation Slip {1}.".format(
+					frappe.bold(self.name),
+					frappe.bold(", ".join(linked))
+				)
+			)
+
+	else:
+		frappe.db.sql("""
+			UPDATE `tabPayment Intimation Slip`
+			SET
+				purchase_order = %s,
+				purchase_order_duplicate = %s
+			WHERE purchase_order = %s
+		""", (
+			self.amended_from,
+			self.amended_from,
+			self.name
+		))
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def on_amend_po(self, method):
+# 	"""
+# 	Code Type 4 (Amend)
+# 	When a Purchase Order is amended, a new PO doc is created with
+# 	'amended_from' pointing to the old (cancelled) PO name.
+# 	Check if that old name exists in any Payment Intimation Slip's
+# 	'purchase_order' or 'purchase_order_duplicate' field.
+# 	If found, update 'purchase_order' with the new amended PO name.
+# 	"""
+# 	if not self.amended_from:
+# 		return
+
+# 	linked = frappe.db.sql("""
+# 		SELECT name FROM `tabPayment Intimation Slip`
+# 		WHERE purchase_order_duplicate = %s OR purchase_order = %s
+# 	""", (self.amended_from, self.amended_from))
+
+# 	if not linked:
+# 		return
+
+# 	frappe.db.sql("""
+# 		UPDATE `tabPayment Intimation Slip`
+# 		SET purchase_order = %s,
+# 		WHERE purchase_order_duplicate = %s OR purchase_order = %s
+# 	""", (self.name, self.amended_from, self.amended_from))
+
+
+
+
+
+
+
+
+
+
+
+
+import frappe
 
 def on_amend_po(self, method):
 	"""
-	Code Type 4 (Amend)
-	When a Purchase Order is amended, a new PO doc is created with
-	'amended_from' pointing to the old (cancelled) PO name.
-	Check if that old name exists in any Payment Intimation Slip's
-	'purchase_order' or 'purchase_order_duplicate' field.
-	If found, update 'purchase_order' with the new amended PO name
-	and clear 'purchase_order_duplicate'.
+	When a Purchase Order is amended:
+	- purchase_order -> New amended PO
+	- purchase_order_duplicate -> Original cancelled PO (amended_from)
 	"""
+
 	if not self.amended_from:
-		return
-
-	linked = frappe.db.sql("""
-		SELECT name FROM `tabPayment Intimation Slip`
-		WHERE purchase_order_duplicate = %s OR purchase_order = %s
-	""", (self.amended_from, self.amended_from))
-
-	if not linked:
 		return
 
 	frappe.db.sql("""
 		UPDATE `tabPayment Intimation Slip`
-		SET purchase_order = %s,
-			purchase_order_duplicate = ''
-		WHERE purchase_order_duplicate = %s OR purchase_order = %s
-	""", (self.name, self.amended_from, self.amended_from))
-
+		SET
+			purchase_order = %s,
+			purchase_order_duplicate = %s
+		WHERE purchase_order = %s
+		   OR purchase_order_duplicate = %s
+	""", (
+		self.name,
+		self.amended_from,
+		self.amended_from,
+		self.amended_from
+	))
 
 
 
@@ -91,12 +180,3 @@ def on_amend_po(self, method):
 # ########### gmp.gmp_machine.doc_event.po_pis_events.on_amend_po
 
 
-
-
-# doc_events = {
-#     "Purchase Order": {
-#         "on_cancel": "gmp.gmp_machine.doc_event.po_pis_events.on_cancel_po",
-#         "after_rename": "gmp.gmp_machine.doc_event.po_pis_events.after_rename_po",
-#         "on_trash": "gmp.gmp_machine.doc_event.po_pis_events.validate_delete_po",
-#     }
-# }
